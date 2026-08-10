d993df88990111d6a27fe042888a0a2c26be4f9b
hemal1020 <hemalahmed1020@gmail.com>
2026-08-10 13:14:13 +0600

    solved problem no 50

---
+++ 50.py
@@ -0,0 +1,11 @@
+import math
+class Solution(object):
+    def judgeSquareSum(self, x, n):
+        return x*x**(n-1)
+              
+
+
+sol = Solution()
+
+
+print(sol.judgeSquareSum(2.00000, 10))   # Test case 2
